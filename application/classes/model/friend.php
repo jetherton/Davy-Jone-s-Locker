e7eb830743764ac098731af42404e534a8f97bf6
John Etherton <john.etherton@gmail.com>
2011-12-12 20:34:01 -0700

    oh yeah, need these files for friends to work.

---
+++ application/classes/model/friend.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_Friend extends ORM {
+
+
+	
+} // End User Model
---
+++ application/views/home/friends_list.php
@@ -0,0 +1,33 @@
+
+
+<table class="list_table">
+	<tr>
+		<th>
+			<?php echo __('friend');?>
+		</th>
+		<th>
+			<?php echo __('groups');?>
+		</th>		
+	</tr>
+	
+	<?php
+		if(count($friends) == 0)
+		{
+			echo '<tr><td colspan="3">'.__('you have no friends').'</td></tr>';
+		}
+		$i = 0;
+		foreach($friends as $friend){
+			$i++;
+			$odd_row = ($i % 2) == 0 ? 'class="odd_row"' : '';
+		?>
+
+	<tr <?php echo $odd_row; ?>>
+		<td>
+			<a href="<?php echo url::base(). 'home/profile/view?id='.$friend->id?>"> <?php echo $friend->first_name . ' ' . $friend->last_name;?></a>
+		</td>
+		<td>
+			Group info
+		</td>
+	</tr>
+	<?php }?>
+</table>
