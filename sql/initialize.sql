f507b4e91cb58a5f82f5f58bf826af95b770a0c7
John Etherton <john.etherton@gmail.com>
2012-07-18 18:14:47 -0600

    tweaing the look with Meredith

--- sql/initialize.sql
+++ sql/initialize.sql
@@ -331,3 +331,14 @@ ALTER TABLE  `updates` CHANGE  `html`  `html` TEXT CHARACTER SET utf8 COLLATE ut
 /** Alter the categories table so that there can be sub levels*/
 ALTER TABLE  `categories` ADD  `parent_id` INT( 11 ) UNSIGNED NOT NULL AFTER  `id` , ADD INDEX (  `parent_id` );
 
+
+/**2012-07-18 - Create the reminders table to remind our users what to do*/
+CREATE TABLE IF NOT EXISTS `reminders` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `type` char(255) NOT NULL, /*more for the admins to set certain kinds of reminders*/
+  `category_id int(11) unsigned NOT NULL DEFAULT '0', /*Leave to zero if this doesn't correspond to a category*/
+  PRIMARY KEY (`id`)  
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+
